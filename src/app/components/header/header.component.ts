0b7e9dfe32e15cdb0f869da5a642e5d39c1ef5f2
vsnegi039 <vipinnegi039@gmail.com>
2024-08-23 22:15:03 +0530

    fix some isssues

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  constructor(public cartService: CartService){};
+export class HeaderComponent implements OnInit {
+  currentRoute: string = '';
+
+  constructor(public cartService: CartService, private router: Router) {}
+
+  ngOnInit() {
+      this.currentRoute = this.router.url;
+      this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.currentRoute = event.urlAfterRedirects;
+        }
+      });
+  }
+
+  isHomePage(): boolean {
+    return this.currentRoute === '/home';
+  }
+
   openCart() {
     this.cartService.openCart();
   }
